9ea3949506eccfe25fb896cfc618f2192ea911fc
wachira <wachiraa954@gmail.com>
2022-07-16 11:20:48 +0300

    destructing appjs props assgn done

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,11 @@ import MovieCard from "./MovieCard";
 
 function App() {
   const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
   return (
     <div className="App">
+      {/* posterSrc is omitted, so the default value will be used instead */}
       <MovieCard title={title} genres={genresArr} />
     </div>
   );
